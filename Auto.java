4dd4c708547ea3ffcc2611bd530f176d99f55637
SamarthRao91 <48369411+SamarthRao91@users.noreply.github.com>
2019-10-14 18:31:53 -0700

    Add files via upload

---
+++ Auto.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+
+public class Auto {
+
+    // todo: write your code here
+    
+    
+    
+    
+    
+    
+    
+    /*
+        What to double:
+        1.in loading zone
+        a pick up sky block
+        b cross team sky bridge
+        c place block on foudation
+        d park on Line 
+        2.in building zone
+        a move foudation
+        b park on line 
+    
+    
+    
+    */
+}
